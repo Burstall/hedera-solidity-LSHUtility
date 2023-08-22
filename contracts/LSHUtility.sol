1b7cf810c6fde48ee10bf7fdd556f0e569d9f23d
Stowerling <richard@alphacenturi.com>
2023-08-22 21:37:49 +0100

    added full suite of methods

--- contracts/LSHUtility.sol
+++ contracts/LSHUtility.sol
@@ -6,10 +6,12 @@ import { SafeHTS } from "./SafeHTS.sol";
 
 contract LSHUtility {
 
+	error InvalidArguments();
+
 	event LSHUtilityEvent (
-		address indexed _sender,
-		uint256 indexed _value,
-		string _message
+		address indexed sender,
+		uint256 indexed value,
+		string message
 	);
 	
 	function checkLiveAllowance(address _token, address _owner, address _spender) public returns (uint256 allowance) {
@@ -21,18 +23,36 @@ contract LSHUtility {
 	}
 
 	function checkLiveAllowances(address[] memory _token, address[] memory _owner, address[] memory _spender) public returns (uint256[] memory allowances) {
-		require(_token.length == _owner.length && _owner.length == _spender.length, "Arrays must be of equal length");
+		if (_token.length != _owner.length || _owner.length != _spender.length) {
+			revert InvalidArguments();
+		}
 		allowances = new uint256[](_token.length);
 		for (uint256 i = 0; i < _token.length; i++) {
-			allowances[i] = SafeHTS.safeAllowance(_token[i], _owner[i], _spender[i]);
+			allowances[i] = checkLiveAllowance(_token[i], _owner[i], _spender[i]);
 		}
 	}
 	
 	function checkTokensApprovedForAllSerial(address[] memory _token, address[] memory _owner, address[] memory _spender) public returns (bool[] memory approvals) {
-		require(_token.length == _owner.length && _owner.length == _spender.length, "Arrays must be of equal length");
+		if (_token.length != _owner.length || _owner.length != _spender.length) {
+			revert InvalidArguments();
+		}
 		approvals = new bool[](_token.length);
 		for (uint256 i = 0; i < _token.length; i++) {
-			approvals[i] = SafeHTS.safeIsApprovedForAll(_token[i], _owner[i], _spender[i]);
+			approvals[i] = isApprovedForAllSerials(_token[i], _owner[i], _spender[i]);
+		}
+	}
+
+	function checkApprovedAddress(address _token, int64 serial) public returns (address approvedAddress) {
+		approvedAddress = SafeHTS.safeGetApproved(_token, serial);
+	}
+
+	function checkApprovedAddresses(address[] memory _token, int64[] memory _serial) public returns (address[] memory approvedAddresses) {
+		if (_token.length != _serial.length) {
+			revert InvalidArguments();
+		}
+		approvedAddresses = new address[](_token.length);
+		for (uint256 i = 0; i < _token.length; i++) {
+			approvedAddresses[i] = checkApprovedAddress(_token[i], _serial[i]);
 		}
 	}
 
